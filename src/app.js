2d08e9a6980af2aa46171edc5a0b90c72b01d6a1
Raquel Moss <raquelxmoss@gmail.com>
2016-10-08 16:51:18 +1300

    Render a board

--- src/app.js
+++ src/app.js
@@ -1,8 +1,56 @@
-import {div} from '@cycle/dom';
+import {svg, pre, h} from '@cycle/dom';
 import xs from 'xstream';
+import _ from 'lodash';
+
+// import LetterBag from './letter-bag';
+
+const initialState = {
+  board: Board()
+};
+
+function debug (value) {
+  return pre('.debug', JSON.stringify(value, null, 2));
+}
+
+function Board () {
+  return _.range(0, 11).map((row) =>
+    _.range(0, 11).map((column) => Square(row, column))
+  );
+}
+
+function Square (row, column) {
+  return {row, column};
+}
+
+function renderSquare (square) {
+  const squareSize = innerWidth / 11;
+
+  return (
+    h('rect', {
+      attrs: {
+        width: squareSize,
+        height: squareSize,
+        x: square.row * squareSize,
+        y: square.column * squareSize,
+        fill: '#272759',
+        stroke: 'aliceblue'
+      }
+    })
+  );
+}
+
+function view (state) {
+  return (
+    h('svg', {attrs: {width: innerWidth, height: innerHeight}}, [
+      ..._.flatten(state.board).map(renderSquare)
+    ])
+  );
+}
 
 export default function App ({DOM, Animation}) {
+  const state$ = xs.of(initialState);
+
   return {
-    DOM: xs.of(div('bello world'))
+    DOM: state$.map(view)
   };
 }
